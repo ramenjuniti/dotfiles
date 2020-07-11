1dc50d7694f9811397bcc3aeaf580dec7dd74928
ramenjuniti <ramenjuniti@gmail.com>
2020-07-12 00:08:45 +0900

    hai

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -4,8 +4,8 @@ set -e
 
 pip install pynvim
 
+# dein.nvim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm ./installer.sh
 
-nvim
