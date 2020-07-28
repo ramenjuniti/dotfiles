5020e8163b20eeab854b87f57683cfe7182ad7de
ramenjuniti <ramenjuniti@gmail.com>
2020-07-28 14:00:05 +0900

    hai

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -3,6 +3,7 @@
 set -e
 
 if type fish >/dev/null 2>&1; then
+	curl https://git.io/fisher --create-dirs -sLo ./.config/fish/functions/fisher.fish
 	fish -c "source $HOME/.config/fish/config.fish"
 	fish -c "fisher"
 fi
